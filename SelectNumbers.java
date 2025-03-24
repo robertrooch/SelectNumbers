061f85c4ac7bff234e379c16ed23e010074dafb3
robertrooch <robertorooch2004@gmail.com>
2025-03-24 01:10:40 -0600

    remove third and four smell code

--- SelectNumbers.java
+++ SelectNumbers.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class SelectNumbers {
     public static void main(String[] args) {
         if (args.length != 3) {
@@ -5,29 +7,18 @@ public class SelectNumbers {
             return;
         }
 
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        int c = Integer.parseInt(args[2]);
+        int[] numbers = new int[3];
 
-        if (a >= b && a >= c) {
-            if (b >= c) {
-                System.out.println(a + ", " + b + ", " + c);
-            } else {
-                System.out.println(a + ", " + c + ", " + b);
+        try {
+            for (int i = 0; i < 3; i++) {
+                numbers[i] = Integer.parseInt(args[i]);
             }
-        } else if (b >= a && b >= c) {
-            if (a >= c) {
-                System.out.println(b + ", " + a + ", " + c);
-            } else {
-                System.out.println(b + ", " + c + ", " + a);
-            }
-        } else {
-            if (a >= b) {
-                System.out.println(c + ", " + a + ", " + b);
-            } else {
-                System.out.println(c + ", " + b + ", " + a);
-            }
+        } catch (NumberFormatException e) {
+            System.out.println("Error: todos los argumentos deben ser enteros.");
+            return;
         }
+
+        Arrays.sort(numbers);
 
         System.exit(0);
     }
