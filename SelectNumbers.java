fff327e618aa0349a8f8ef43aa041d164e510fdc
robertrooch <robertorooch2004@gmail.com>
2025-03-24 00:26:29 -0600

    remove first smell code

--- SelectNumbers.java
+++ SelectNumbers.java
@@ -1,6 +1,4 @@
 public class SelectNumbers {
-    public static int numero = 42;
-
     public static void main(String[] args) {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
